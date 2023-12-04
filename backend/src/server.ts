f461d3ada048ffcd5f5c393c8649925980770cc8
Lahcen JOU <lahcen.jou@eviden.com>
2023-12-04 15:12:29 +0100

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+import express from "express";
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello, World")
+});
+
+
+const port = process.env.PORT;
+
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+.then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+        console.log("Server running on port: " + port);
+    });
+});
+
